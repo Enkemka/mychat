b115ac61479ec06730cc7fe2fd600de8bc11c15a
essemoo <enkemka@montgomerycollege.edu>
2025-06-05 14:16:27 -0400

    need to finish controller

--- src/main/java/com/example/mychat/MychatApplication.java
+++ src/main/java/com/example/mychat/MychatApplication.java
@@ -1,4 +1,4 @@
-package com.example.mychat.admin;
+package com.example.mychat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
